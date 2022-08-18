2cdcf92b817a24dc923b41fcb96f7f44b041843f
vladislav <vladiksmart@gmail.com>
2022-08-18 03:08:56 +0300

    Added statuses and types consts and methods in operation model

--- app/Models/Operation.php
+++ app/Models/Operation.php
@@ -17,18 +17,62 @@ class Operation extends Model
     private const TYPE_TOP_UP = 'top_up';
     private const TYPE_PAYOUT = 'payout';
 
+    private const PENDING_STATUS = 'pending';
+    private const FAILED_STATUS = 'failed';
+    private const SUCCEEDED_STATUS = 'succeeded';
+    private const CANCELLED_STATUS = 'cancelled';
+
+    public static function STATUSES()
+    {
+        return [
+            self::PENDING_STATUS,
+            self::FAILED_STATUS,
+            self::SUCCEEDED_STATUS,
+            self::CANCELLED_STATUS,
+        ];
+    }
+
+    public static function TYPES()
+    {
+        return [
+            self::TYPE_SUBSCRIPTION_PAYMENT,
+            self::TYPE_TOP_UP,
+            self::TYPE_PAYOUT
+        ];
+    }
+
     public static function TYPE_SUBSCRIPTION_PAYMENT()
     {
-        return self::TYPE_SUBSCRIPTION_PAYMENT();
+        return self::TYPE_SUBSCRIPTION_PAYMENT;
     }
 
     public static function TYPE_TOP_UP()
     {
-        return self::TYPE_TOP_UP();
+        return self::TYPE_TOP_UP;
     }
 
     public static function TYPE_PAYOUT()
     {
-        return self::TYPE_PAYOUT();
+        return self::TYPE_PAYOUT;
+    }
+
+    public static function PENDING_STATUS()
+    {
+        return self::PENDING_STATUS;
+    }
+
+    public static function FAILED_STATUS()
+    {
+        return self::FAILED_STATUS;
+    }
+
+    public static function SUCCEEDED_STATUS()
+    {
+        return self::SUCCEEDED_STATUS;
+    }
+
+    public static function CANCELLED_STATUS()
+    {
+        return self::CANCELLED_STATUS;
     }
 }
